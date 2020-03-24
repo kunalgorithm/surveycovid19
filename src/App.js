a5f3dd49c21f8c4583da80124b84fd76dd8cab23
Kunal <me@kunal.sh>
2020-03-23 17:42:28 -0700

    import fetch call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Papa from "papaparse";
+// import {csv } from "d3"
 
 function App() {
+  const [rows, setRows] = React.useState([]);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("/data/MasterExcel2.csv");
+      const reader = response.body.getReader();
+      const result = await reader.read(); // raw array
+      const decoder = new TextDecoder("utf-8");
+      const csv = decoder.decode(result.value); // the csv text
+      const results = Papa.parse(csv, { header: true }); // object with { data, errors, meta }
+      const rows = results.data; // array of objects
+      console.log(rows);
+      setRows(rows);
+    }
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +28,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {rows.length}
         <a
           className="App-link"
           href="https://reactjs.org"
